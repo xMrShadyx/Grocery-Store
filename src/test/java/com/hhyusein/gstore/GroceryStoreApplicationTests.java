b16f6941ebaaf0fcd23b18a0c31c58c1ed335a1b
xmrshadyx <daredevil91138@gmail.com>
2022-03-19 18:41:37 +0200

    Initial Commit

---
+++ src/test/java/com/hhyusein/gstore/GroceryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhyusein.gstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
